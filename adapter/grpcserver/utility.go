74a479e5639d08c6937ff290b941249b5752d80a
John-Alan Simmons <simmons.johnalan@gmail.com>
2024-04-01 05:35:13 -0400

    Implemented new utility funcs

--- adapter/grpcserver/utility.go
+++ adapter/grpcserver/utility.go
@@ -10,19 +10,25 @@ import (
 
 	"github.com/sourcenetwork/orbis-go/app"
 	utilityv1alpha1 "github.com/sourcenetwork/orbis-go/gen/proto/orbis/utility/v1alpha1"
+	"github.com/sourcenetwork/orbis-go/pkg/cosmos"
 	"github.com/sourcenetwork/orbis-go/pkg/crypto"
 	"github.com/sourcenetwork/orbis-go/pkg/pre/elgamal"
 
+	"github.com/sourcenetwork/sourcehub/x/acp/types"
+
+	ssicrypto "github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/TBD54566975/ssi-sdk/did/key"
+	"github.com/cometbft/cometbft/crypto/tmhash"
+	"github.com/cosmos/cosmos-sdk/types/bech32"
+	prototypes "github.com/cosmos/gogoproto/types"
 	"github.com/go-jose/go-jose/v3"
 	"github.com/go-jose/go-jose/v3/jwt"
+	ic "github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/samber/do"
 	"go.dedis.ch/kyber/v3"
 	"go.dedis.ch/kyber/v3/suites"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	ssicrypto "github.com/TBD54566975/ssi-sdk/crypto"
-	"github.com/TBD54566975/ssi-sdk/did/key"
-	ic "github.com/libp2p/go-libp2p/core/crypto"
 )
 
 type utilService struct {
@@ -62,6 +68,39 @@ func (s *utilService) CreateDID(ctx context.Context, req *utilityv1alpha1.Create
 	}
 
 	return resp, nil
+}
+
+func (s *utilService) CreateBech32Address(ctx context.Context, req *utilityv1alpha1.CreateBech32AddressRequest) (*utilityv1alpha1.CreateBech32AddressResponse, error) {
+	if req.PublicKey == nil {
+		return nil, status.Error(codes.InvalidArgument, "public key and key type are required")
+	}
+
+	if req.Prefix == "" {
+		return nil, status.Error(codes.InvalidArgument, "bech32 prefix required")
+	}
+
+	buf := tmhash.SumTruncated(req.PublicKey)
+	addr, err := bech32.ConvertAndEncode(req.Prefix, buf)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "bech32 encoding: %s", err)
+	}
+
+	return &utilityv1alpha1.CreateBech32AddressResponse{
+		Bech32Address: addr,
+	}, nil
+
+	/*
+		func (pubKey *PubKey) Address() crypto.Address {
+			if len(pubKey.Key) != PubKeySize {
+				panic("pubkey is incorrect size")
+			}
+			// For ADR-28 compatible address we would need to
+			// return address.Hash(proto.MessageName(pubKey), pubKey.Key)
+			return crypto.Address(tmhash.SumTruncated(pubKey.Key))
+		}
+
+		bech32.ConvertAndEncode(prefix, addr)
+	*/
 }
 
 func (s *utilService) CreateJWT(ctx context.Context, req *utilityv1alpha1.CreateJWTRequest) (*utilityv1alpha1.CreateJWTResponse, error) {
@@ -215,6 +254,82 @@ func (s *utilService) DecryptSecret(ctx context.Context, req *utilityv1alpha1.De
 	}
 
 	return resp, nil
+}
+
+func (s *utilService) AuthzRegisterObject(ctx context.Context, req *utilityv1alpha1.AuthzRegisterObjectRequest) (*utilityv1alpha1.AuthzRegisterObjectResponse, error) {
+	cc, err := do.Invoke[*cosmos.Client](s.app.Injector())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "missing cosmos client")
+	}
+
+	if req.ObjectId == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing object ID")
+	}
+	if req.ObjectResource == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing object resource")
+	}
+	if req.PolicyId == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing policy ID")
+	}
+	if req.Creator == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing creator")
+	}
+
+	registerMsg := &types.MsgRegisterObject{
+		Creator:  req.Creator,
+		PolicyId: req.PolicyId,
+		Object: &types.Object{
+			Resource: req.ObjectResource,
+			Id:       req.ObjectId,
+		},
+		CreationTime: prototypes.TimestampNow(),
+	}
+
+	_, err = cc.BroadcastTx(ctx, cc.Account, registerMsg)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "broadcast tx: %s", err)
+	}
+
+	return nil, nil
+}
+
+func (s *utilService) AuthzSetRelationship(ctx context.Context, req *utilityv1alpha1.AuthzSetRelationshipRequest) (*utilityv1alpha1.AuthzSetRelationshipResponse, error) {
+	cc, err := do.Invoke[*cosmos.Client](s.app.Injector())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "missing cosmos client")
+	}
+
+	if req.ObjectId == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing object ID")
+	}
+	if req.ObjectResource == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing object resource")
+	}
+	if req.Creator == "" {
+		return nil, status.Error(codes.InvalidArgument, "missing creator")
+	}
+	if req.Actor == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing actor")
+	}
+	if req.Relation == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing relation")
+	}
+	if req.PolicyId == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "missing policy ID")
+	}
+
+	relationship := types.NewActorRelationship(req.ObjectResource, req.ObjectId, req.Relation, req.Actor)
+	setRelmsg := types.NewMsgSetRelationshipNow(
+		req.Creator,
+		req.PolicyId,
+		relationship,
+	)
+	_, err = cc.BroadcastTx(ctx, cc.Account, setRelmsg)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "broadcast tx: %s", err)
+	}
+
+	return nil, nil
 }
 
 func kyberSuiteToSSIKeyType(kt string) (ssicrypto.KeyType, error) {
