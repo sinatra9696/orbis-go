0b73e3922833d5f70866d6de7db2a10f0ddac427
John-Alan Simmons <simmons.johnalan@gmail.com>
2024-04-03 18:21:42 -0400

    Updated crypto config

--- config/config.go
+++ config/config.go
@@ -76,9 +76,9 @@ type Bulletin struct {
 
 type Host struct {
 	Crypto struct {
-		Type string `default:"ed25519" description:"crypto type"`
+		Type string `default:"secp256k1" description:"crypto type"`
 		Bits int    `default:"-1" description:"crypto bits, if selectable"`
-		Seed int    `default:"0" description:"crypto seed"`
+		Seed int    `default:"0" description:"crypto seed (TESTING ONLY)"`
 	}
 	ListenAddresses []string `default:"/ip4/0.0.0.0/tcp/9000" description:"Host listen address string"`
 	BootstrapPeers  []string `mapstructure:"bootstrap_peers" default:"" description:"Comma separated multiaddr strings of bootstrap peers. If empty, the node will run in bootstrap mode"`
